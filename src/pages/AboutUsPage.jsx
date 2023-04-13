e87428eab39af949f2b1b47034ffd360a3385e15
unknown <sergio.otero@labinfo.is.escuelaing.edu.co>
2023-04-13 15:50:56 -0500

    fron

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,15 +1,18 @@
+import { Input, FormControl , Flex, Heading, Divider, UnorderedList, ListItem, Center, Button} from '@chakra-ui/react'
 export function AboutUs() {
     return <section>
-        <h2>Quienes somos?</h2>
-        <p>Esta aplicacion funciona para organizar facilmente las tareas de todos los dias. Siempre tenemos muchas cosas de hacer, necesitamos algo que nos permita saber tareas pendientes.</p>
-        <h3>Funcionalidades:</h3>
-        <ul>
-            <li> Crear tareas</li>
-            <li> Eliminar Tareas</li>
-            <li> Editar tareas</li>
-            <li> Terminar tareas</li>
-            <li> Activar tareas</li>
-        </ul>
+        <Flex direction="column" borderBottomRadius="50px" bgColor="#ffd66f">
+            <Heading>Quienes somos?</Heading>
+            <p>Esta aplicacion funciona para organizar facilmente las tareas de todos los dias. Siempre tenemos muchas cosas de hacer, necesitamos algo que nos permita saber tareas pendientes.</p>
+            <h3>Funcionalidades:</h3>
+            <ul>
+                <li> Crear tareas</li>
+                <li> Eliminar Tareas</li>
+                <li> Editar tareas</li>
+                <li> Terminar tareas</li>
+                <li> Activar tareas</li>
+            </ul>
+        </Flex>
     </section>;
 }
   
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
+import { Input, FormControl , Flex, Heading, Divider, UnorderedList, ListItem, Center, Button} from '@chakra-ui/react'
 export function HomePage() {
-    return <section>
-            <h2>Bienvenido a la app de Todo List!!!</h2>
-        </section>;
+    return <Flex direction="column" borderBottomRadius="50px" bgColor="#ffd66f">
+                <Heading padding="2%">Bienvenido a la app de Todo List!!!</Heading>
+            </Flex>
   }
   
